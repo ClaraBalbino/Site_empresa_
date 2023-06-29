d12ba20200d974774f271b06faf508bc845eec5f
Clara_Balbino <110998737+ClaraBalbino@users.noreply.github.com>
2023-06-29 11:21:40 -0300

    site

--- Site_Oficial_empresa/JS/script.js
+++ Site_Oficial_empresa/JS/script.js
@@ -23,6 +23,7 @@ document.querySelectorAll('.products-container .product').forEach(product =>{
   };
 });
 
+
 previewBox.forEach(close =>{
   close.querySelector('.fa-times').onclick = () =>{
     close.classList.remove('active');
